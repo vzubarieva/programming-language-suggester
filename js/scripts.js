56d7769353140ab9dbaae6007a5f87029b73c999
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-08 22:59:50 -0700

    add funtionality to the survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
+    const question1 = $("input:radio[name=question1]:checked").val();
+    const question2 = $("input:radio[name=question2]:checked").val();
+    const question3 = $("input:radio[name=question3]:checked").val();
+    const question4 = $("input:radio[name=question4]:checked").val();
+    const question5 = $("input:radio[name=question5]:checked").val();
+    let result;
+    if (question1 === "yes") {
+       result = "Your first programming language is C#";
+    } else if (question2, question5 === "yes") {
+       result = "Your first best programming language to learn is JavaScript";
+    } else  {
+       result = "Your best programming language to learn is Ruby";
+    }
+    $("#output").text(result);
+  });
+});
