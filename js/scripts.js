e0687f47bb036ae3c25ba3541fbeceafb7a1fa9f
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-04-09 23:05:17 -0700

    fix condition for JavaScript case

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $("form#survey").submit(function(event) {
+$(document).ready(function () {
+  $("form#survey").submit(function (event) {
     event.preventDefault();
     const question1 = $("input:radio[name=question1]:checked").val();
     const question2 = $("input:radio[name=question2]:checked").val();
@@ -8,11 +8,11 @@ $(document).ready(function() {
     const question5 = $("input:radio[name=question5]:checked").val();
     let result;
     if (question1 === "yes") {
-       result = "Your first best programming language to learn is C#";
-    } else if (question2, question5 === "yes") {
-       result = "Your first best programming language to learn is JavaScript";
-    } else  {
-       result = "Your first best programming language to learn is Ruby";
+      result = "Your first best programming language to learn is C#";
+    } else if (question2 === "yes" && question5 === "yes") {
+      result = "Your first best programming language to learn is JavaScript";
+    } else {
+      result = "Your first best programming language to learn is Ruby";
     }
     $("#output").text(result);
   });
